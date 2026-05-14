d24ee8b6981faa5bb4b5133dec0876a432c5161c
rohan <rowan201303@gmail.com>
2026-05-13 17:43:56 -0700

    pip install automated

--- main/run.bat
+++ main/run.bat
@@ -3,6 +3,11 @@ cd /d "%~dp0"
 echo ======================================================
 echo   STARTING PLAYGROUND WEB SERVER ENVIRONMENT         
 echo ======================================================
+
+echo -------- installing libs --------
+pip install minecraft-launcher-lib
+echo ---------------------------------
+
 start "" "http://localhost:8000/student-playground.html"
 python server_backend.py
 pause
